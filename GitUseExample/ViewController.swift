db72f77692d90bbaa59d9691a1ee379119d4b08c
Neil Clavin <neilclavin@de3mcl-505739.ber.global>
2017-09-06 16:53:34 +0200

    Initial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by Neil Clavin on 06.09.17.
+//  Copyright © 2017 Neil Clavin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
