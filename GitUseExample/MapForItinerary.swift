5b85b34c82275e6ea87edbf121f2da4af4ad3fc1
Neil Clavin <neilclavin@neil-clavin.ttg.global>
2017-09-06 17:10:55 +0200

    added map branch

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Neil Clavin on 06.09.17.
+//  Copyright © 2017 Neil Clavin. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+    
+}
